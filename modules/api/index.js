74eead8fb6da506f675c7c49822a194231ad9141
mzabriskie <mzabriskie@gmail.com>
2014-10-07 13:28:06 -0600

    Basic light blinking

--- modules/api/index.js
+++ modules/api/index.js
@@ -1,9 +1,27 @@
+var axios = require('axios');
 var argv = require('minimist')(process.argv.slice(2));
 var port = argv.p || 3000;
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var server;
+var LIFX_HOST = 'http://192.168.1.223:56780';
+var LIFX_ID = 'd073d5018475';
+
+function toggleBrightness(color, count) {
+	console.log('toggle brightness', count);
+	axios.put(LIFX_HOST + '/lights/' + LIFX_ID + '/color', {
+		hue: color.hue,
+		saturation: color.saturation,
+		brightness: (count) % 2,
+		duration: .01
+	})
+	.then(function () {
+		if (count < 5) {
+			toggleBrightness(color, count + 1);
+		}
+	});
+}
 
 server = http.createServer(function (req, res) {
 	var url = req.url;
@@ -11,6 +29,15 @@ server = http.createServer(function (req, res) {
 	if (url === '/api/knock') {
 		console.log('Knock knock Neo');
 		res.write('Knock knock Neo');
+		res.end();
+	} else if (url === '/api/lifx') {
+		axios.get(LIFX_HOST + '/lights/' + LIFX_ID)
+			.then(function (res) {
+				toggleBrightness(res.data.color, 0);
+			});
+
+		console.log('Ooh, pretty');
+		res.write('Ooh, pretty');
 		res.end();
 	} else {
 		res.write('Unsupported request');
